38f02b112fa73030101ea96eebc6bf0d1c6dcbdc
Felipe Santos <felipefss@gmail.com>
2022-06-29 22:32:01 +0100

    Separate app in Sidebar and Content components.

--- 01.1-desafio-componentes/src/components/SideBar.tsx
+++ 01.1-desafio-componentes/src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { Button } from './Button';
+
+import { GenreResponseProps } from '../App';
+
+import { api } from '../services/api';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  onSelectGenre: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ onSelectGenre, selectedGenreId }: SideBarProps) {
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onSelectGenre(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
