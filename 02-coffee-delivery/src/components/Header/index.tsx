387c7530c9ed2f0f6457c834565ca35af2a6b55e
Felipe Santos <felipefss@gmail.com>
2022-10-06 23:02:50 +0100

    Add link to Checkout page on Header.

--- 02-coffee-delivery/src/components/Header/index.tsx
+++ 02-coffee-delivery/src/components/Header/index.tsx
@@ -1,21 +1,24 @@
 import { MapPin, ShoppingCart } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 import { Button } from '../Button';
 import * as Styled from './styles';
 
 export function Header() {
   return (
     <Styled.HeaderContainer>
-      <a href="/">
+      <Link to='/'>
         <img src='assets/Logo.svg' alt="Copo take-out de café escrito Coffee Delivery ao lado" />
-      </a>
+      </Link>
       <Styled.ActionButtons>
         <Button variant='location'>
           <MapPin weight='fill' size={22} />
           <span>Porto Alegre, RS</span>
         </Button>
-        <Button variant='cart'>
-          <ShoppingCart weight='fill' size={22} />
-        </Button>
+        <Link to='/checkout'>
+          <Button variant='cart'>
+            <ShoppingCart weight='fill' size={22} />
+          </Button>
+        </Link>
       </Styled.ActionButtons>
     </Styled.HeaderContainer>
   );
