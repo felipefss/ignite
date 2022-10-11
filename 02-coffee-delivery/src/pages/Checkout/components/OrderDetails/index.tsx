abd6bd2bd1712ab3739be021659861be6261cfc8
Felipe Santos <felipefss@gmail.com>
2022-10-11 23:30:57 +0100

    Make order details to update accordingly.

--- 02-coffee-delivery/src/pages/Checkout/components/OrderDetails/index.tsx
+++ 02-coffee-delivery/src/pages/Checkout/components/OrderDetails/index.tsx
@@ -1,11 +1,19 @@
 import { useContext } from 'react';
+
 import { Button } from '../../../../components/Button';
-import { CartContext } from '../../../../contexts/CartContext';
 import { CoffeeSelected } from '../CoffeeSelected';
+import { CartContext } from '../../../../contexts/CartContext';
+import { formatToReais } from '../../../../utils/format';
+
 import * as Styled from './styles';
+
+const DELIVERY_RATE = 3.5;
 
 export function OrderDetails() {
   const { cart } = useContext(CartContext);
+
+  const subTotal = cart.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+  const total = subTotal + DELIVERY_RATE;
 
   return (
     <Styled.OrderContainer>
@@ -26,17 +34,17 @@ export function OrderDetails() {
       <Styled.Total>
         <Styled.TotalLine>
           <span>Total de itens</span>
-          <span>R$ 29,70</span>
+          <span>{formatToReais(subTotal)}</span>
         </Styled.TotalLine>
 
         <Styled.TotalLine>
           <span>Entrega</span>
-          <span>R$ 3,50</span>
+          <span>{formatToReais(DELIVERY_RATE)}</span>
         </Styled.TotalLine>
 
         <Styled.TotalLine>
           <span>Total</span>
-          <span>R$ 33,20</span>
+          <span>{formatToReais(total)}</span>
         </Styled.TotalLine>
       </Styled.Total>
 
