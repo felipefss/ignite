e98b172143f8a2ab1f9d1610018f6ae912922f47
Felipe Santos <felipefss@gmail.com>
2023-02-21 22:38:08 +0000

    Add Counter and Search input

--- 03-github-blog/src/pages/Blog/index.tsx
+++ 03-github-blog/src/pages/Blog/index.tsx
@@ -6,6 +6,30 @@ export function Blog() {
   return (
     <Styled.Container>
       <Profile />
+
+      <Styled.CounterContainer>
+        <Styled.CounterTitle>Publicações</Styled.CounterTitle>
+        <Styled.Counter>6 publicações</Styled.Counter>
+      </Styled.CounterContainer>
+
+      <form action="#">
+        <Styled.SearchInput type="text" placeholder="Buscar conteúdo" />
+      </form>
+
+      <section>
+        <div>
+          <header>
+            <span>Javascript data types and data structures</span>
+            <span>Há 1 dia</span>
+          </header>
+
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corrupti consequuntur reprehenderit dolorem
+            ipsam, culpa autem dicta. Debitis optio dolor beatae a possimus totam, eaque commodi, eius, corporis
+            dignissimos fugiat.
+          </span>
+        </div>
+      </section>
     </Styled.Container>
   );
 }
--- 03-github-blog/src/pages/Blog/styles.ts
+++ 03-github-blog/src/pages/Blog/styles.ts
@@ -4,4 +4,43 @@ export const Container = styled.main`
   width: 100%;
   max-width: 864px;
   margin: 0 auto 0;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CounterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  margin: 4.5rem 0 0.75rem;
+`;
+
+export const CounterTitle = styled.span`
+  font-size: 1.125rem;
+  font-weight: 700;
+  color: ${(props) => props.theme.subtitle};
+`;
+
+export const Counter = styled.span`
+  font-size: 0.875rem;
+  color: ${(props) => props.theme.span};
+`;
+
+export const SearchInput = styled.input`
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.input};
+  border: 1px solid ${(props) => props.theme.border};
+  border-radius: 6px;
+  width: 100%;
+  padding: 0.75rem 1rem;
+
+  &::placeholder {
+    color: ${(props) => props.theme.label};
+  }
+
+  &:focus {
+    outline: solid ${(props) => props.theme.blue};
+  }
 `;
